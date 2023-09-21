39db16b814667b93e958e91358223362c1a66549
SuperSaish <129974287+SuperSaish@users.noreply.github.com>
2023-09-21 12:45:07 +0530

    Update master_slave.v

--- master_slave.v
+++ master_slave.v
@@ -1,1 +1,27 @@
-module
+module master_slave(
+  input J,K,clk,pr,clr,
+  output reg Q,Qbar
+);
+  wire w1,w2,w3,w4;
+  always@(clk) begin
+    if(pr) begin
+      Q<=1;
+      Qbar<=0;
+    end
+    else if(clr) begin
+      Q<=0;
+      Qbar<=1;
+    end
+    else begin
+      w1 <= ~(J & clk & Qbar);
+      w2 <= ~(K & clk & Q);
+      w3 <= ~(Qmbar & w1);
+      w4 <= ~(Qm & w2);
+      w5 <= ~(w3 & (~clk));
+      w6 <= ~(w4 & (~clk));
+      Q <= ~(Qbar & pr & w5);
+      Qbar <= ~(Qbar & clr & w6);
+    end
+  end
+endmodule
+    
